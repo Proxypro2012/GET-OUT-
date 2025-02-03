d7109971900a316caebf97e27cba7ec8384a0ffd
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-03 13:50:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import streamlit_shadcn_ui as ui
 
 if 'cart' not in st.session_state:
     st.session_state.cart = []
 
 
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
+r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
 options = []
 
 
@@ -15,12 +17,16 @@ options = []
 options.extend(["Home", "My Cart"])
 selected_page = st.sidebar.radio("Navigation Menu",options=options)
 
-with r1col2:  
-  if selected_page == options[0]:
-    st.title("Home Page")
 
-with r1col2:
-  if selected_page == options[1]:
+if selected_page == options[0]:
+ with r1col2:  
+     st.title("Home Page")
+ with r2col2:
+     
+    
+
+if selected_page == options[1]:
+   with r1col2: 
     st.title("My Cart")
 
 
