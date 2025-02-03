362d77b84c2c0c79c012df748df5696f5f729e2b
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-03 13:30:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
-
+r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
 options = []
 
+
+
+
+
+
 options.extend(["Home", "My Cart"])
-st.sidebar.radio(options=options)
+selected_page = st.sidebar.radio("Navigation Menu",options=options)
+
+with r1col2:  
+  if selected_page == options[0]:
+    st.title("Home Page")
+
+with r1col2:
+  if selected_page == options[1]:
+    st.title("My Cart")
 
 
