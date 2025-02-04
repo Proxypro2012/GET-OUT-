b6c0ea26e67ee1a70be2763c1a1fd43be0e78614
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-04 11:25:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if 'cart' not in st.session_state:
 r1col1, r1col2, r1col3 = st.columns([1, 2, 1])
 r2col1, r2col2, r2col3 = st.columns([1, 2, 1])
 r3col1, r3col2, r3col3 = st.columns([1, 2, 1])
-r4col1, r4col2, r4col3 = st.columns([1, 2, 1.5])
+r4col1, r4col2, r4col3 = st.columns([1, 2, 1.25])
 r5col1, r5col2, r5col3 = st.columns([1, 2, 1])
 
 options = []
